94771af36310fa3b7eda9379ad7a8be09a59456f
tipreethund <tipreethund@gmail.com>
2023-07-06 15:23:59 -0600

    first push files

---
+++ src/firstpage/Firstclass.java
@@ -0,0 +1,30 @@
+package firstpage;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+    int a=20,b=10;
+    int sum=a+b;
+    int sub=a-b;
+    int mul=a*b;
+    int div=a/b;
+    int mod=a%b;
+    System.out.println("sum="+sum);
+    System.out.println("sub="+sub);
+    System.out.println("mul="+mul);
+    System.out.println("div="+div);
+    System.out.println("mod="+mod);
+    System.out.println(a--);//post decrement
+    System.out.println(a);
+    System.out.println(--a);//pre decrement
+    System.out.println(a);
+    System.out.println(a++);//post increment
+    System.out.println(a);
+    System.out.println(++a);//pre increment
+    System.out.println(a);
+    
+    
+	
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author srava
+ *
+ */
+module practice1 {
+}
